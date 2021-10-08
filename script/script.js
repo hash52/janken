609657fdbf731cf957acdafd5a6ff00c0f9388c2
Hash52 <hirokinoakaunto@gmail.com>
2021-10-08 22:14:28 +0900

    refactor: 重複した文字列を定数にした

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
 let playerScore = 0;
 let computerScore = 0;
+
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const compSelect = document.getElementById('computerSelect');
 const playerSelect = document.getElementById('playerSelect');
 const message = document.getElementById('message');
-let gameActive = false;
+const victoryCondition = document.getElementById('victory-condition');
+
+const VICTORY_SCORE = 5;
+
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
+const PLAYER = 'player';
+const COM = 'computer';
+
+const WINNER_COLOR = 'green';
+const LOSER_COLOR = 'red';
+
+const PLAYER_WIN = 'Player1の勝ち！';
+const COM_WIN = 'Computerの勝ち！';
+const DRAW = 'あいこ';
 
 function computerPlay() {
   let arr = [1, 2, 3];
@@ -13,90 +30,98 @@ function computerPlay() {
   let value;
   switch (random) {
     case 1:
-      value = 'rock';
+      value = ROCK;
       break;
     case 2:
-      value = 'paper';
+      value = PAPER;
       break;
     default:
-      value = 'scissors';
+      value = SCISSORS;
   }
   return value;
 }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return 'あいこ';
-  } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
-    return "Player1の勝ち！";
-  } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
-    return "Player1の勝ち！";
-  } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
-    return "Player1の勝ち！";
-  } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
-    return "Computerの勝ち！";
-  } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
-    return "Computerの勝ち！";
-  } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
-    return "Computerの勝ち！";
+    return DRAW;
+  } else if ((playerSelection == ROCK) && (computerSelection == SCISSORS)) {
+    return PLAYER_WIN;
+  } else if ((playerSelection == PAPER) && (computerSelection == ROCK)) {
+    return PLAYER_WIN;
+  } else if ((playerSelection == SCISSORS) && (computerSelection == PAPER)) {
+    return PLAYER_WIN;
+  }else if ((playerSelection == ROCK) && (computerSelection == PAPER)) {
+    return COM_WIN;
+  } else if ((playerSelection == PAPER) && (computerSelection == SCISSORS)) {
+    return COM_WIN;
+  } else if ((playerSelection == SCISSORS) && (computerSelection == ROCK)) {
+    return COM_WIN;
   }
 }
 
-function displaySelection(player, selection, result) {
-  if (player === 'player') {
-    playerSelect.innerHTML = `<i class="fas fa-hand-${selection}"></i>`;
-    playerSelect.style.color = '';
-  } else {
-    compSelect.innerHTML = `<i class="fas fa-hand-${selection}"></i>`;
-    compSelect.style.color = '';
+function displaySelection(playerType, selection) {
+  let displayed;
+  switch(playerType){
+    case PLAYER:
+      displayed = playerSelect;
+      break;
+    case COM:
+      displayed = compSelect;
+      break;
   }
+  displayed.innerHTML = `<i class="fas fa-hand-${selection}"></i>`;
+  displayed.style.color = '';
 }
 
 function displayResult(playerSelection,computerSelection,result){
   playerSelect.innerHTML = `<i class="fas fa-hand-${playerSelection}"></i>`;
   compSelect.innerHTML = `<i class="fas fa-hand-${computerSelection}"></i>`;
-  if (result === "Player1の勝ち！") {
-    playerSelect.style.color = 'green';
-    compSelect.style.color = 'red';
+  if (result === PLAYER_WIN) {
+    playerSelect.style.color = WINNER_COLOR;
+    compSelect.style.color = LOSER_COLOR;
   }
-  if (result === "Computerの勝ち！") {
-    compSelect.style.color = 'green';
-    playerSelect.style.color = 'red';
+  if (result === COM_WIN) {
+    compSelect.style.color = WINNER_COLOR;
+    playerSelect.style.color = LOSER_COLOR;
   }
-  if (result === 'あいこ') {
+  if (result === DRAW) {
     compSelect.style.color = '';
     playerSelect.style.color = '';
   }
 }
 
-function resetSlection(){
+function resetDisplay(){
   playerSelect.innerHTML = '';
   compSelect.innerHTML = '';
   playerSelect.style.color = '';
   compSelect.style.color = '';
+  message.innerHTML = '　';
 }
 
 function scoreBoard(result) {
-  if (result === "Player1の勝ち！") {
-    playerScore++;
-    pScore.innerText = playerScore;
-    cScore.innerText = computerScore;
-  } else if (result === "Computerの勝ち！") {
-    computerScore++;
-    pScore.innerText = playerScore;
-    cScore.innerText = computerScore;
-  } else {
-    return false;
+  switch(result){
+    case PLAYER_WIN:
+      playerScore++;
+      pScore.innerText = playerScore;
+      cScore.innerText = computerScore;
+      break;
+    case COM_WIN:
+      computerScore++;
+      pScore.innerText = playerScore;
+      cScore.innerText = computerScore;
+      break;
+    case DRAW:
+      break;
   }
 }
 
 function endGame() {
-  return playerScore === 5 || computerScore === 5;
+  return playerScore === VICTORY_SCORE || computerScore === VICTORY_SCORE;
 }
 
 function whoWon() {
-  if (playerScore === 5) {
-    message.innerText = 'Player1の勝利！おめでとう！'
+  if (playerScore === VICTORY_SCORE) {
+    message.innerText = 'おめでとう！Player1の勝利！'
   } else {
     message.innerText = 'Computerの勝利！残念でした〜'
   }
@@ -118,7 +143,8 @@ function initBoards() {
   select.style.display = 'block';
   pScore.innerText = playerScore;
   cScore.innerText = computerScore;
-  gameActive = true;
+  victoryCondition.innerHTML = `先に${VICTORY_SCORE}勝した方が勝利！`
+  message.innerHTML = '　'; //１行確保するために空白を入れておく
 }
 
 function wait(ms) {
@@ -153,17 +179,29 @@ startGameButton.addEventListener('click', (e)=>{
 });
 
 async function gameFlow(){
-  while(!endGame()){
-    resetSlection();
+  //ゲームが終わるまで繰り返す ※ while = ~している間
+  while(!endGame()){ // ※ ! = ~じゃない時 (コンピュータ語)
+    //画面の表示をリセット
+    resetDisplay();
+    //コンピュータが手を選択
     let computerSelection = computerPlay();
-    displaySelection('computer', computerSelection, null);
+    //コンピュータの手の表示
+    displaySelection(COM, computerSelection);
+    //プレイヤーが手を選ぶまで一時停止
     let playerSelection = await Promise.any([selectRock(), selectPaper(), selectScissors()]);
+    //じゃんけんの結果を取得 ※ playRound = ひと勝負する
     let result = playRound(playerSelection, computerSelection);
+    //じゃんけんの結果を表示
     displayResult(playerSelection, computerSelection, result);
+    //スコアの表示
     scoreBoard(result);
+    //メッセージの表示
     message.innerText = result;
+    //1.5秒一時停止
     await wait(1500);
   }
+  //勝った人を表示する
   whoWon();
+  //ページを読み込み直して最初の画面に戻る
   reload();
 }
