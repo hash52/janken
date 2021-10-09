ef55d51736629f116c465912ba469b6360e3531c
Hash52 <hirokinoakaunto@gmail.com>
2021-10-09 19:59:28 +0900

    アーケードモード（勝ち抜きバトル）

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 //外部モジュール化はサーバーを立てないと不可
 class Charactor{
-  constructor(name, imagePath, life, lv, type){
+  constructor(name, imagePath, iconPath, defeatedIconPath, life, lv, type){
       this.name = name;
       this.face = imagePath;
+      this.icon = iconPath;
+      this.defeatedIcon = defeatedIconPath;
       this.life = life;
       this.lv = lv;
       this.type = type;
@@ -38,6 +40,12 @@ const compSelect = document.getElementById('computerSelect');
 const playerSelect = document.getElementById('playerSelect');
 const message = document.getElementById('message');
 
+let comIcons = [
+  document.getElementById('com0'),
+  document.getElementById('com1'),
+  document.getElementById('com2'),
+]
+
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
@@ -49,9 +57,21 @@ const CHARACTOR_ASSET_PATH = `${ASSET_PATH}charactors/`;
 const HEART = `<img src=${ASSET_PATH}heart.jpg class='col img-fluid p-0'>`;
 const HEART_EMPTY = `<img src=${ASSET_PATH}heart-empty.jpg class='col img-fluid p-0'>`;
 const HEART_HALF = `<img src=${ASSET_PATH}heart-half.jpg class='col img-fluid p-0'>`;
+const COL = `<div class="col p-0"></div>`;
 
-const player = new Charactor("タケミッチ", `${CHARACTOR_ASSET_PATH}takemichi/face.jpg`, 3, 1, Charactor.PLAYER);
-const com = new Charactor("佐野万次郎", `${CHARACTOR_ASSET_PATH}maiki/face.jpg`, 3, 50, Charactor.COM);;
+const MAX_LIFE_NUM = 7;
+
+const player = new Charactor("タケミッチ", `${CHARACTOR_ASSET_PATH}takemichi/face.jpg`, `${CHARACTOR_ASSET_PATH}maiki/icon.png`, `${CHARACTOR_ASSET_PATH}maiki/defeated_icon.png`, 3, 1, Charactor.PLAYER);
+const coms = [
+  new Charactor("ザコ１", `${CHARACTOR_ASSET_PATH}maiki/face.jpg`, `${CHARACTOR_ASSET_PATH}maiki/icon.png`, `${CHARACTOR_ASSET_PATH}maiki/defeated_icon.png`, 1, 49, Charactor.COM),
+  new Charactor("ザコ２", `${CHARACTOR_ASSET_PATH}maiki/face.jpg`, `${CHARACTOR_ASSET_PATH}maiki/icon.png`, `${CHARACTOR_ASSET_PATH}maiki/defeated_icon.png`, 1, 49, Charactor.COM),
+  new Charactor("佐野万次郎", `${CHARACTOR_ASSET_PATH}maiki/face.jpg`, `${CHARACTOR_ASSET_PATH}maiki/icon.png`, `${CHARACTOR_ASSET_PATH}maiki/defeated_icon.png`, 1, 49, Charactor.COM),
+];
+
+let stage = 0;
+let com;
+
+
 
 const WINNER_COLOR = 'green';
 const LOSER_COLOR = 'red';
@@ -147,8 +167,12 @@ function reflectLifeGuageBy(result) {
   }
 }
 
-function endGame() {
-  return player.life == 0 || com.life == 0;
+function endGame(){
+  return player.life <= 0 || coms.length < stage;
+}
+
+function endThisGame() {
+  return player.life <= 0 || com.life == 0;
 }
 
 function whoWon() {
@@ -176,35 +200,49 @@ function displayLifeGauge(charactor){
       break;
   }
   let lifeGauge = ``;
+  let lifeGaugeLength = 0;
   for(let i = 1; i <= charactor.life; i++){
     lifeGauge += HEART;
+    lifeGaugeLength++;
   }
   if(!Number.isInteger(charactor.life)){
     lifeGauge += HEART_HALF;
+    lifeGaugeLength++;
   }
   for(let i = 1; i <= charactor.getLostLifeInThisGame(); i++){
     lifeGauge += HEART_EMPTY;
+    lifeGaugeLength++;
+  }
+  for(let i = 0; i < MAX_LIFE_NUM - lifeGaugeLength; i++){
+    lifeGauge += COL;
   }
   displayed.innerHTML = lifeGauge;
 }
 
-async function initBoards() {
+function initBoards() {
   const start = document.getElementById('start');
   const boards = document.getElementById('boards');
   const select = document.getElementById('select');
-  const comName = document.getElementById('com-name');
-  const comImg = document.getElementById('com-img');
-
+  const comlist = document.getElementById('com-list');
   start.style.display = 'none';
   boards.style.display = 'block';
   select.style.display = 'block';
+  comlist.style.display = 'block';
+  message.innerHTML = '　'; //１行確保するために空白を入れておく
+}
+
+async function updateBoard(){
+  const comName = document.getElementById('com-name');
+  const comImg = document.getElementById('com-img');
+  comName.innerHTML = '？？？';
+  comImg.src = `${ASSET_PATH}/question.jpg`;
+  com = coms[stage];
   displayLifeGauge(player);
   displayLifeGauge(com);
-  message.innerHTML = '　'; //１行確保するために空白を入れておく
-
-  await wait(1000);
-  comName.innerHTML += com.name;
+  await wait(1500);
+  comName.innerHTML = com.name;
   comImg.src = com.face;
+  comIcons[stage].src = com.icon;
 }
 
 function wait(ms) {
@@ -240,19 +278,24 @@ startGameButton.addEventListener('click', (e)=>{
 
 async function gameFlow(){
   while(!endGame()){
-    resetSelectionDisplay();
-    player.selection = await Promise.any([selectRock(), selectPaper(), selectScissors()]);
-    displaySelection(player);
-    com.selection = computerPlay();
-    displaySelection(com);
-    message.innerHTML += 'ぽんっ！';
-    await wait(1000);
-    let result = playRound(player, com);
-    displaySelectionsBy(result);
-    reflectLifeGuageBy(result);
-    message.innerText = result;
-    await wait(2000);
+    await updateBoard();
+    while(!endThisGame()){
+      resetSelectionDisplay();
+      player.selection = await Promise.any([selectRock(), selectPaper(), selectScissors()]);
+      displaySelection(player);
+      com.selection = computerPlay();
+      displaySelection(com);
+      message.innerHTML += 'ぽんっ！';
+      await wait(1000);
+      let result = playRound(player, com);
+      displaySelectionsBy(result);
+      reflectLifeGuageBy(result);
+      message.innerText = result;
+      await wait(2000);
+    }
+    comIcons[stage].src = com.defeatedIcon;
+    whoWon();
+    stage++;
   }
-  whoWon();
   reload();
 }
